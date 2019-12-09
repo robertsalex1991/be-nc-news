36c30691a2cffc3693d9513ea91621fff0182cd8
robertsalex1991 <roberts.alex1991@gmail.com>
2019-12-09 16:48:13 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 const { apiRouter } = require("./routes/api_router");
 const {
